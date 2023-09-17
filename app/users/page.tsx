095859cdb3c4d79aff071d6f200d6a2215291880
Chalermpong Dejnarong <chalermpong.d@arise.tech>
2023-09-17 11:54:16 +0700

    Static and Dynamic rendering with caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,13 +7,15 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    next: { revalidate: 10 },
+    next: { revalidate: 5 }, // cache for 5 second
+    cache: 'no-store', // no caching
   });
 
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
