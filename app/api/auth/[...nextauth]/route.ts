8c3e145b6088d674f1c3a3ee8f6792d2b84ef078
Chalermpong Dejnarong <chalermpong.d@arise.tech>
2023-09-17 20:39:15 +0700

    how to credential

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,47 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+import { mock } from 'node:test';
 
 export const authOptions = {
   providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'Email' },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'password',
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        // call api for check authorize for username
+        const mockUser = {
+          id: '1',
+          name: 'jay',
+          email: 'mail@mail.com',
+          password: 'password',
+          hashedPassword: 'password',
+        };
+        const user = mockUser;
+        if (!user) return null;
+        // const passwordMatch = await bcrypt.compare(
+        //   credentials.password,
+        //   user.hashedPassword
+        // );
+        let passwordMatch = false;
+        if (
+          credentials.email === user.email &&
+          credentials.password === user.password
+        )
+          passwordMatch = true;
+        return passwordMatch ? user : null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
