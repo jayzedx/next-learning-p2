d952ea66ceed9cd1850540c5e84830c1edaa9310
Chalermpong Dejnarong <chalermpong.d@arise.tech>
2023-09-17 16:35:16 +0700

    handling error and custom error page

---
+++ app/global-error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const ErrorPage = () => {
+  return <div>An unexpected error has occured (global).</div>;
+};
+
+export default ErrorPage;
---
+++ app/users/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const UserErrorPage = () => {
+  return <div>An unexpected error has occured (user).</div>;
+};
+
+export default UserErrorPage;
